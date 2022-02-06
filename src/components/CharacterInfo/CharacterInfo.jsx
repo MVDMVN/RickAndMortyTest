196c0c4fc74a3dbebf34d1bca5c1bb8fa8d0b0d5
Andrey Melnichuk <edifens@gmail.com>
2022-02-06 20:21:56 +0200

    small fixes

--- src/components/CharacterInfo/CharacterInfo.jsx
+++ src/components/CharacterInfo/CharacterInfo.jsx
@@ -2,7 +2,7 @@ import './CharacterInfo.scss'
 import axios from 'axios'
 import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
-import { Container } from '@mui/material'
+import { Container, Typography } from '@mui/material'
 
 const baseURL = 'https://rickandmortyapi.com/api/character'
 
@@ -23,11 +23,51 @@ function CharacterInfo() {
   }
 
   return (
-    <Container sx={{ pt: 3, pb: 40 }} className="App">
-      <p>{character.name}</p>
-      <img src={character.image} alt={`${character.name} Character`} />
-      <p>{character.status}</p>
-      <p>{character.species}</p>
+    <Container sx={{ pt: 3, pb: 40 }}>
+      <Typography variant="h1" textAlign="center" mb="24px">
+        {character.name}
+      </Typography>
+      <div className="character-info">
+        <div className="character-details">
+          <p>
+            <b>Name:</b> {character.name}
+          </p>
+          <p>
+            <b>Species:</b> {character.species}
+          </p>
+          <p>
+            <b>Gender:</b> {character.gender}
+          </p>
+          <p>
+            <b>Location:</b> {character.location.name}
+          </p>
+          <p>
+            <b>Status:</b> {character.status}
+          </p>
+          <p>
+            <b>Created:</b>{' '}
+            {`
+            ${character.created.substr(0, 10)} 
+            ${character.created.substr(11, 8)}
+            `}
+          </p>
+        </div>
+        <div className="character-photo">
+          <img src={character.image} alt={`${character.name} Character`} />
+        </div>
+      </div>
+      <Typography variant="h3" mb="12px">
+        Episodes:{' '}
+      </Typography>
+      <div className="episodes">
+        {character.episode.map((item) => {
+          return (
+            <a href={item} key={item}>
+              {item}
+            </a>
+          )
+        })}
+      </div>
     </Container>
   )
 }
